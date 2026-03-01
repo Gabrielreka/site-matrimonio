fc128eca3eb9da076f61f5c4a7eef7c113ef7ffa
Gabrielreka <joaogabrielpilote@gmail.com>
2026-02-28 21:30:16 -0300

    Trocando os nomes no site

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Aqui você define os nomes que vão aparecer no site
-    return render_template('site.html', nome1="Naty", nome2="Vampiro")
+    return render_template('site.html', nome1="João Tomaz", nome2="Elaine Cristina")
 
 if __name__ == '__main__':
     app.run(debug=True)
